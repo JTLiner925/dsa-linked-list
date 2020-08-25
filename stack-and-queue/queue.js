14635ee35902aa8438e2b53747aed4c78ec7fa40
JTLiner925 <[jtliner925@gmail.com]>
2020-08-25 12:52:18 -0500

    queue and stack

---
+++ stack-and-queue/queue.js
@@ -0,0 +1,51 @@
+class _Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null; 
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    if (this.first === null){
+      this.first = node;
+    }
+    if (this.last){
+      this.last.next = node;
+    }
+    this.last = node;
+  }
+
+
+  dequeue(){
+    if(this.first === null){
+      return;
+    }
+    const node = this.first;
+    this.first = this.first.next;
+
+    if(node === this.last){
+      this.last = null;
+    }
+    return node.value;
+  }
+}
+function main() {
+  let starTrekQ = new Queue();
+
+  starTrekQ.enqueue('Kirk');
+  starTrekQ.enqueue('Spock');
+  starTrekQ.enqueue('Uhura');
+  starTrekQ.enqueue('Sulu');
+  starTrekQ.enqueue('Checkov');
+  
+
+}
+main();
---
+++ stack-and-queue/stack.js
@@ -0,0 +1,57 @@
+class _Node {
+  constructor(data, next){
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+  }
+
+  push(data) {
+    if(this.top === null){
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
+
+  peek(data){
+    if(this.top === null){
+      return 'nothing in this stack';
+    }
+    else(console.log(this.top.data));
+  }
+
+  isEmpty(){
+    if(this.top !== null){
+      return;
+    } else {
+      console.log('stack is empty');
+    }
+  }
+}
+
+function main() {
+  let starTrek = new Stack();
+  let myTrek = new Stack();
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+  starTrek.peek();
+  myTrek.isEmpty();
+  console.log(starTrek);
+  console.log(myTrek);
+
+}
+main();
